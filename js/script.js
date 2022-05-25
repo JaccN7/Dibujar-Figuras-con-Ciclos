8c424db174083b954f21ae48c97815258f4ae9ec
Javiera Cortés <javieracortesceledon@gmail.com>
2022-05-25 11:06:41 -0400

    actualizado

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 //Dibujar una X con ciclos
-var n = 5;
+var n = 10;
 var y = (n - 1);
 document.write("Dibujar X <br/>");
 for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
         if ((i == j) || ((i + j) == y)) {
-            document.write(".");
+            document.write("*");
         }
         else {
-            document.write("&nbsp");
+            document.write("&nbsp ");
         }
     }document.write('<br/>')
 }
@@ -18,15 +18,15 @@ var x = 1;
 document.write("<br/>Dibujar Z <br/>");
 for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-        if ((i == 0) || (i == y)) {
-            document.write(".");
+        if ((i == 0) || (i == (n-1))) {
+            document.write("*");
         }
         else if (j == (y - x)){
-            document.write(".");
+            document.write("*");
             x++;
-            console.log(x)
+            console.log(n)
         }else {
-            document.write("&nbsp");
+            document.write("&nbsp ");
         }
     }document.write('<br/>')
 }
